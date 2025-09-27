9f4d0a98a3f7ec69f59603b57cc388cef0d04549
akazawaya <ktakimotondsinc@gmail.com>
2025-09-27 15:45:11 +0900

    Tutorial 5: Relationships & Hyperlinked APIs 終了

--- tutorial/snippets/serializers.py
+++ tutorial/snippets/serializers.py
@@ -2,16 +2,18 @@ from rest_framework import serializers
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 from django.contrib.auth.models import User
 
-class SnippetSerializer(serializers.ModelSerializer):
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight', format='html')
+
     # 表示用の“シリアライズ項目”を定義しているだけで,モデル（DBスキーマ）に変更はない
     class Meta:
         model = Snippet
-        fields = ['id', 'title', 'code', 'linenos', 'language', 'style', 'owner' ]
+        fields = ['url', 'id', 'owner', 'style', 'highlight', 'title', 'code', 'linenos', 'language']
 
 
-class UserSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.HyperlinkedModelSerializer):
     snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
     class Meta:
         model = User
-        fields = ['id', 'username', 'snippets']
+        fields = ['url', 'id', 'username', 'snippets']
